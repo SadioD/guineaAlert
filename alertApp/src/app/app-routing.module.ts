f31cf201a6d00bd5c5c232cfb370108bfcfe956b
Sadio DIALLO <amasadio85@gmail.com>
2018-09-12 04:11:04 +0200

    style: formatage de code

--- alertApp/src/app/app-routing.module.ts
+++ alertApp/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   { path: ''                ,  redirectTo:   'home'         , pathMatch: 'full' },
-  { path: 'authentification',  component:    AuthComponent },
   { path: 'home'    ,          loadChildren: './app-view/home/home.module#HomePageModule' },
   { path: 'settings',          loadChildren: './app-view/settings/settings.module#SettingsPageModule' }
 ];
