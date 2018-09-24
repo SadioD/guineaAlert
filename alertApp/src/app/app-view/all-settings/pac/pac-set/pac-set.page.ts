91bd1122ff61d7988e22b36fe51b1b87b3fb5936
Sadio DIALLO <amasadio85@gmail.com>
2018-09-24 03:38:52 +0200

    style(settings-pac): ajout de la page pac-list qui liste les pac. J'ai du inclurer le code jquery directement dans le pac-set.page.ts au lieu d'importer depuis src/asset, car le back-button de la toolbar empechait le fichier de charger

--- alertApp/src/app/app-view/all-settings/pac/pac-set/pac-set.page.ts
+++ alertApp/src/app/app-view/all-settings/pac/pac-set/pac-set.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import '../../../../../assets/js/pac-set.page.js';
+// import '../../../../../assets/js/pac-set.page.js';
 
+declare var $: any;
 @Component({
   selector: 'app-pac-set',
   templateUrl: './pac-set.page.html',
@@ -13,11 +14,37 @@ export class PacSetPage implements OnInit {
 
     // Initialise les dépendances de la page
     ngOnInit() {
-
+       this.loadJqueryCode();
     }// ------------------------------------------------------------------------------------------------------------------------------------
     // METHODES ---------------------------------------------------------------------------------------------------------------------------
     // Exploite les données à la soumission du formulaire
     onSubmit(form: NgForm) {
         console.log(form.value);
+    }// ------------------------------------------------------------------------------------------------------------------------------------
+    // SCRIPTS ----------------------------------------------------------------------------------------------------------------------------
+    // Charge du code Jquery directement dans le typeScript (Import ne marche pas lorsqu'on ajoute le backButton)
+    loadJqueryCode() {
+        $(function () {
+            const manager = {
+                // Désactive le boutton d'ajout de PAC si les champs Inputs sont vides
+                disableSubmit: function () {
+                    if (!$('#pseudo').val() || !$('#firstName').val() || !$('#tel').val()) {
+                        $('#submitButton').attr('disabled', true);
+                    }
+                },
+                // Customise la couleur des labels lors du focus
+                styleForm: function () {
+                    $('#pseudo, #firstName, #tel').focus(function (e) {
+                        $(e.target).prev().css('color', 'black');
+                    });
+                },
+                // initialise le script
+                loadScript: function () {
+                    manager.disableSubmit();
+                    manager.styleForm();
+                }
+            };
+            manager.loadScript();
+        });
     }// ------------------------------------------------------------------------------------------------------------------------------------
 }
--- alertApp/src/app/services/settings.service.ts
+++ alertApp/src/app/services/settings.service.ts
@@ -10,45 +10,47 @@ export class SettingsService {
     // VARIABLES + CONSTR -----------------------------------------------------------------------------------------------------------------
     // Table userSettings (liste des fonctionnalités avec les options saved pas l'User)
     userSettings: UserSettings = new UserSettings({
-            id: 1,
-            hash: 'huui65ugu',  // Créer un hash pour différencier les gens ayant le même nom
-            pseudo: 'Ahmed',
-            firstName: 'DIALLO',
-            settings: {
-                pacList: [
-                    {
-                        firstName: 'BAH',
-                        pseudo: 'Grasset',
-                        tel: 664463486
-                    },
-                    {
-                        firstName: 'BAH-DIALLO',
-                        pseudo: 'Hope',
-                        tel: 622221239
-                    }
-                ]
+        id: 1,
+        hash: 'huui65ugu',  // Créer un hash pour différencier les gens ayant le même nom
+        pseudo: 'Ahmed',
+        firstName: 'DIALLO',
+        email: 'dialloamadou1@yahoo.fr',
+        password: '*******',
+        pacList: [
+            {
+                firstName: 'BAH',
+                pseudo: 'Grasset Mariam',
+                tel: 664463486
+            },
+            {
+                firstName: 'BAH-DIALLO',
+                pseudo: 'Hope Binta',
+                tel: 622221239
             }
-
-        });
+        ]
+    });
     // Table settings (Liste des fonctionnalités de l'App)
     settingsList: Settings = new Settings([
         {
-            name:       'Personnes A Contacter (PAC)',
-            shortDesc:  'Modifier votre liste de personnes à contacter',
-            longDesc:   'Lorem Ipsum Et quia Mesopotamiae tractus omnes crebro inquietari sueti praetenturis et stationibus servabantur agrariis, laevorsum flexo itinere Osdroenae subsederat extimas partes, novum parumque aliquando temptatum commentum adgressus.quod si impetrasset, fulminis modo cuncta vastarat.erat autem quod cogitabat huius modi.',
-            url:        '/settings/pac-list'
+            name: 'Personnes A Contacter (PAC)',
+            shortDesc: 'Modifier votre liste de personnes à contacter',
+            longDesc: 'Lorem Ipsum Et quia Mesopotamiae tractus omnes crebro inquietari sueti praetenturis et stationibus servabantur agrariis, laevorsum flexo itinere Osdroenae subsederat extimas partes, novum parumque aliquando temptatum commentum adgressus.quod si impetrasset, fulminis modo cuncta vastarat.erat autem quod cogitabat huius modi.',
+            url: '/settings/pac-list',
+            icon: 'people'
         },
         {
-            name:       'Fonction ',
-            shortDesc:  'Modifier votre liste de personnes à contacter',
-            longDesc:   'Lorem Ipsum Et quia Mesopotamiae tractus omnes crebro inquietari sueti praetenturis et stationibus servabantur agrariis, laevorsum flexo itinere Osdroenae subsederat extimas partes, novum parumque aliquando temptatum commentum adgressus.quod si impetrasset, fulminis modo cuncta vastarat.erat autem quod cogitabat huius modi.',
-            url:        '/settings/pac-list'
+            name: 'Fonction ',
+            shortDesc: 'Modifier votre liste de personnes à contacter',
+            longDesc: 'Lorem Ipsum Et quia Mesopotamiae tractus omnes crebro inquietari sueti praetenturis et stationibus servabantur agrariis, laevorsum flexo itinere Osdroenae subsederat extimas partes, novum parumque aliquando temptatum commentum adgressus.quod si impetrasset, fulminis modo cuncta vastarat.erat autem quod cogitabat huius modi.',
+            url: '/settings/pac-list',
+            icon: 'cog'
         },
         {
-            name:       'Surprise',
-            shortDesc:  'Modifier votre liste de personnes à contacter',
-            longDesc:   'Lorem Ipsum Et quia Mesopotamiae tractus omnes crebro inquietari sueti praetenturis et stationibus servabantur agrariis, laevorsum flexo itinere Osdroenae subsederat extimas partes, novum parumque aliquando temptatum commentum adgressus.quod si impetrasset, fulminis modo cuncta vastarat.erat autem quod cogitabat huius modi.',
-            url:        '/settings/pac-list'
+            name: 'Surprise',
+            shortDesc: 'Modifier votre liste de personnes à contacter',
+            longDesc: 'Lorem Ipsum Et quia Mesopotamiae tractus omnes crebro inquietari sueti praetenturis et stationibus servabantur agrariis, laevorsum flexo itinere Osdroenae subsederat extimas partes, novum parumque aliquando temptatum commentum adgressus.quod si impetrasset, fulminis modo cuncta vastarat.erat autem quod cogitabat huius modi.',
+            url: '/settings/pac-list',
+            icon: 'cog'
         },
     ]);
     // Les Subjects (Settings référencés[name, short/long Desc, etc.] + Settings que l'user a saved[PAC, raccourcis alert icones, etc.])
